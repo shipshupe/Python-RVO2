f042a933576121157c51177a04c5c1e5d4fe55bc
rshupe <ryan.shupe@gmail.com>
2019-01-30 21:43:42 -0800

    worked in windows but broke in unix, fix for that?

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 from Cython.Build import cythonize
+from os import name as os_name
 
+if os_name == 'nt':
+    #windows requires static links for exported symbols
+    ext_list = ['src/*.pyx', 'src/rvo2.cpp', 'src/RVOSimulator.cpp',  'src/KdTree.cpp', 'src/Obstacle.cpp', 'src/Agent.cpp']
+else:
+    #unix is smarter
+    ext_list = ['src/*.pyx']
 
 class BuildRvo2Ext(_build_ext):
     """Builds RVO2 before our module."""
@@ -15,19 +22,20 @@ class BuildRvo2Ext(_build_ext):
         build_dir = os.path.abspath('build/RVO2')
         if not os.path.exists(build_dir):
             os.makedirs(build_dir)
-            subprocess.check_call(['cmake', '../..', '-DCMAKE_CXX_FLAGS=-fPIC'],
+            subprocess.check_call(['cmake', '../..'],
                                   cwd=build_dir)
         subprocess.check_call(['cmake', '--build', '.', '--config', 'Release'], cwd=build_dir)
 
         _build_ext.run(self)
 
 
+
 extensions = [
-    Extension('rvo2', ['src/*.pyx', 'src/RVOSimulator.cpp', 'src/rvo2.cpp', 'src/KdTree.cpp', 'src/Obstacle.cpp', 'src/Agent.cpp'],
+    Extension('rvo2', ext_list,
               include_dirs=['src'],
               libraries=['RVO'],
               library_dirs=['build/RVO2/src', 'build/RVO2/src/Release'],
-              extra_compile_args=['-fPIC']),
+              extra_compile_args=[]),
 ]
 
 setup(
